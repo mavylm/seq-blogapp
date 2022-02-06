bd891c3d0e89ff789fe95bed90c816daa283834c
Mavis Lim <81512500+mavylm@users.noreply.github.com>
2022-02-07 03:50:20 +0800

    Updated styles

--- config/config.js
+++ config/config.js
@@ -6,20 +6,20 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST_NAME,
-    dialect: process.env.DB_DIALECT,
+    dialect: process.env.DB_DIALECT
   },
   test: {
     username: 'root',
     password: null,
     database: 'database_test',
     host: '127.0.0.1',
-    dialect: 'mysql',
+    dialect: 'mysql'
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST_NAME,
-    dialect: process.env.DB_DIALECT,
-  },
+    dialect: process.env.DB_DIALECT
+  }
 };
